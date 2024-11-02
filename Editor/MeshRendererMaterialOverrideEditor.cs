e4c9b51329512baa2216b2173460450373cb93c3
Fred Moreau <frederic.moreau@unity3d.com>
2024-11-02 06:27:31 +0100

    method breakdown

--- Editor/MeshRendererMaterialOverrideEditor.cs
+++ Editor/MeshRendererMaterialOverrideEditor.cs
@@ -30,47 +30,73 @@ namespace Unity.LocalMaterialVariants.Editor
 
         public override void OnInspectorGUI()
         {
-            var sharedMaterials = meshRenderer.sharedMaterials;
-
             ExpandOverrides = EditorGUILayout.BeginFoldoutHeaderGroup(ExpandOverrides, "Overrides");
+
             if (ExpandOverrides)
-            {
-                EditorGUI.BeginChangeCheck();
-                for (int i = 0; i < sharedMaterials.Length; ++i)
-                {
-                    if (sharedMaterials[i] == null)
-                        continue;
+                MaterialOverridesGUI();
 
-                    bool isOverriden = string.IsNullOrEmpty(AssetDatabase.GetAssetPath(sharedMaterials[i]));
-
-                    EditorGUI.BeginChangeCheck();
-                    bool overrideMaterial = GUILayout.Toggle(isOverriden, isOverriden ? sharedMaterials[i].parent.name : sharedMaterials[i].name, EditorStyles.miniButton);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        Undo.RecordObject(meshRenderer, isOverriden ? "Revert Renderer Material" : "Override Renderer Material");
-                        if (overrideMaterial)
-                        {
-                            var variant = new Material(sharedMaterials[i]);
-                            variant.name = $"{meshRenderer.gameObject.name}.{sharedMaterials[i].name}";
-                            variant.parent = sharedMaterials[i];
-                            sharedMaterials[i] = variant;
-                        }
-                        else
-                        {
-                            if (sharedMaterials[i].parent == null)
-                                Debug.LogWarning("Local Material has no parent.");
-                            else
-                                sharedMaterials[i] = sharedMaterials[i].parent;
-                        }
-                    }
-                }
-
-                if (EditorGUI.EndChangeCheck())
-                    meshRenderer.sharedMaterials = sharedMaterials;
-            }
             EditorGUILayout.EndFoldoutHeaderGroup();
 
             defaultEditor.OnInspectorGUI();
+        }
+
+        private void MaterialOverridesGUI()
+        {
+            var sharedMaterials = meshRenderer.sharedMaterials;
+
+            EditorGUI.BeginChangeCheck();
+
+            for (int i = 0; i < sharedMaterials.Length; ++i)
+            {
+                if (sharedMaterials[i] == null)
+                    continue;
+
+                MaterialGUI(ref sharedMaterials[i]);
+            }
+
+            if (EditorGUI.EndChangeCheck())
+                meshRenderer.sharedMaterials = sharedMaterials;
+        }
+
+        private void MaterialGUI(ref Material material)
+        {
+            bool isOverriden = IsLocal(material);
+
+            EditorGUI.BeginChangeCheck();
+            bool overrideMaterial = GUILayout.Toggle(isOverriden, isOverriden ? material.parent.name : material.name, EditorStyles.miniButton);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(meshRenderer, isOverriden ? "Revert Renderer Material" : "Override Renderer Material");
+                if (overrideMaterial)
+                {
+                    MakeLocalVariant(ref material, meshRenderer.gameObject.name);
+                }
+                else
+                {
+                    RevertToParent(ref material);
+                }
+            }
+        }
+
+        private static void RevertToParent(ref Material material)
+        {
+            if (material.parent == null)
+                Debug.LogWarning("Local Material has no parent.");
+            else
+                material = material.parent;
+        }
+
+        private static void MakeLocalVariant(ref Material material, string prefix = default)
+        {
+            var variant = new Material(material);
+            variant.name = $"{prefix}.{material.name}";
+            variant.parent = material;
+            material = variant;
+        }
+
+        private static bool IsLocal(Material material)
+        {
+            return string.IsNullOrEmpty(AssetDatabase.GetAssetPath(material));
         }
     }
 }
